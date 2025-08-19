06423dff532ae83ddf5ef86768e5b1be1c4a86cf
Gerard Rey <gerard@echiverri.net>
2025-08-19 13:17:37 -0700

    feat: update job

--- src/app/api/webhooks/jobs/route.ts
+++ src/app/api/webhooks/jobs/route.ts
@@ -1,4 +1,4 @@
-import { createShipment, getShipment, updateShipment } from "@/services/shipments";
+import { createShipment, getShipment, getShipmentByJob, updateShipment } from "@/services/shipments";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -8,19 +8,22 @@ export async function POST(request: NextRequest) {
     } else {
         // Process the valid webhook payload
         console.log("Received valid webhook:", body);
-        const { job, shipment, ...data } = body;
-        let record = await getShipment(shipment || job);
+        const { job, ...data } = body;
+        let record = await getShipmentByJob(job);
 
-        if (!record) {
+        if (!record && body.shipment) {
             const newRecord = await createShipment(body);
             if (!newRecord.error) {
                 record = newRecord;
             } else {
                 return NextResponse.json({ error: newRecord.details || newRecord.message }, { status: 400 });
             }
-        } else if (data.latitude || data.longitude || data.status) {
-            record = await updateShipment(shipment, data);
+        } else if (record && (data.latitude || data.longitude || data.status)) {
+            record = await updateShipment(record.shipment, data);
         }
+
+        if (!record)
+            return NextResponse.json({ error: 'Job not found', ...body }, { status: 200 });
         
         return NextResponse.json(record, { status: 200 });
     }
