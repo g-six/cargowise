a15a0e3cdc56221aa2dbfa8db3d49d0e32ebcd27
Gerard Rey <gerard@echiverri.net>
2025-09-05 23:10:49 -0700

    feat: check in wip

--- src/app/(app)/my/teams/team-grid.tsx
+++ src/app/(app)/my/teams/team-grid.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { Stat } from '@/app/stat'
-import { useAppContext, useAppDispatch } from '../../context-provider'
+import { useAppContext } from '../../context-provider'
 import { Team } from '@/data';
 
 export default function TeamGrid() {
 	const ctx = useAppContext()
-    const dispatch = useAppDispatch();
-    console.table(ctx?.teams)
     return (
 		<div className="mt-4 grid gap-8 sm:grid-cols-2 xl:grid-cols-4">
 			{ctx?.teams.map((t: Team & { athlete_count?: number }) => (
